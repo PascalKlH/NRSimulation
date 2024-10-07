95446340cc334249f875096f91a548c9fd1da540
PascalKlH <130033640+PascalKlH@users.noreply.github.com>
2024-10-07 13:35:58 +0200

    Delete simproject directory

--- simproject/simapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Home page
-    path('run_simulation/', views.run_simulation, name='run_simulation'),  # URL to trigger the script
-    path('plot_simulation/', views.plot_simulation, name='plot_simulation'),
-]
